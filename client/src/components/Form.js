6ff00cc39c541339f2481a440e466ecba2c03ba1
emcintire <everettgsm@gmail.com>
2021-03-07 22:47:35 -0500

    Added confirm password field

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -3,26 +3,32 @@ import React,{ Component } from 'react'
 class Form extends Component{ 
 constructor(props){ 
 	super(props) 
-	this.state = { email:'',name:'', password:'' } 
+	this.state = { email:'',name:'', password:'', confirmPassword:'' } 
 	this.handleChange = this.handleChange.bind(this) 
 	this.handleSubmit = this.handleSubmit.bind(this) 
 } 
 
 handleSubmit = async event => {
-    event.preventDefault();
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 
-        name: this.state.name,
-        email: this.state.email,
-        password: this.state.password
-      }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
+	event.preventDefault();
+
+	if (this.state.password !== this.state.confirmPassword) {
+		alert("Passwords don't match");
+	}
+	else {
+		const response = await fetch('/api/users', {
+		  method: 'POST',
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify({ 
+			name: this.state.name,
+			email: this.state.email,
+			password: this.state.password
+		  }),
+		});
+		const body = await response.text();
+		this.setState({ responseToPost: body });
+	}
 };
 
 handleChange(event){ 
@@ -35,15 +41,6 @@ render(){
 	return( 
 	<form onSubmit={this.handleSubmit}> 
 		<div> 
-		<label htmlFor='email'>Email</label> 
-		<input 
-			name='email'
-			placeholder='Email'
-			value = {this.state.email} 
-			onChange={this.handleChange} 
-		/> 
-		</div> 
-		<div> 
 		<label htmlFor='name'>Name</label> 
 		<input 
 			name='name'
@@ -53,11 +50,29 @@ render(){
 		/> 
 		</div> 
 		<div> 
+		<label htmlFor='email'>Email</label> 
+		<input 
+			name='email'
+			placeholder='Email'
+			value = {this.state.email} 
+			onChange={this.handleChange} 
+		/> 
+		</div> 
+		<div> 
 		<label htmlFor='password'>Password</label> 
 		<input 
 			name='password'
 			placeholder='Password'
 			value={this.state.password} 
+			onChange={this.handleChange} 
+		/> 
+		</div> 
+		<div> 
+		<label htmlFor='confirmPassword'>Confirm Password</label> 
+		<input 
+			name='confirmPassword'
+			placeholder='confirmPassword'
+			value={this.state.confirmPassword} 
 			onChange={this.handleChange} 
 		/> 
 		</div> 
--- server/index.js
+++ server/index.js
@@ -5,15 +5,6 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/validation")();
 
-app.use(function(req, res, next){
-    console.log(`${req.method} requrest for ${req.url}`);
-    next();
-});
-
-app.get('/getjson', function(req, res){
-    res.json(data);
-});
-
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
