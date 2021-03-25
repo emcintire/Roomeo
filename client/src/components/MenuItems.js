5df0acc86fe0efd95c00a466f38d27c81b60964d
Adora-A <aasin001@plattsburgh.edu>
2021-03-25 16:48:41 -0400

    added dropdown menu function and menu items

---
+++ client/src/components/MenuItems.js
@@ -0,0 +1,18 @@
+export const MenuItems = [
+    {
+        title: 'Edit profile',
+        path: '/UserProfile',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Account settings',
+        path: '/UserAccount',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Sign out',
+        path: '/',
+        cName: 'dropdown-link'
+    },
+
+];
