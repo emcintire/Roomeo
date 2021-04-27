79877894c061c9904b225523b43698b32880b692
Adora-A <aasin001@plattsburgh.edu>
2021-04-27 02:58:34 -0400

    Updated home page

--- client/src/components/pages/HomePage/Home.js
+++ client/src/components/pages/HomePage/Home.js
@@ -2,18 +2,13 @@ import React from 'react'
 import HeroSection from '../../HeroSection'
 import {
   homeObjOne, 
-  // homeObjTwo, 
-//   homeObjThree, 
-  // homeObjFour
 } from './Data'
 
 function Home() {
     return (
         <>
           <HeroSection {...homeObjOne} />
-          {/* <HeroSection {...homeObjThree} />   */}
-          {/* <HeroSection {...homeObjTwo} />   */}
-          {/* <HeroSection {...homeObjFour} />     */}
+
         </>
     );
 }
