6e9e30648872784e5c04d79ddd28c2ba1d5a786b
Adora-A <aasin001@plattsburgh.edu>
2021-04-27 18:33:30 -0400

    edited privacy page

--- client/src/components/pages/Privacy/privacy.js
+++ client/src/components/pages/Privacy/privacy.js
@@ -4,11 +4,27 @@ import React from 'react';
 function Privacy() {
     return (
         <div> 
-            <h1> Privacy Policy</h1>
-            <p>IMPORTANT: BY USING THE SITE OR THE OFFERINGS AVAILABLE ON THIS SITE, YOU GIVE YOUR CONSENT THAT ALL PERSONAL DATA THAT YOU SUBMIT MAY BE PROCESSED BY US IN THE MANNER AND FOR THE PURPOSES DESCRIBED BELOW. IF YOU DO NOT AGREE TO THESE TERMS AND CONDITIONS, DO NOT USE THE SITE.
-
-                1. Types of Information We Collect
-                In order to better provide you with the functionality of the Roommmates.com website, we collect two types of information about our users: Personally Identifiable Information ("PII") and Aggregate Information.
+            <br></br>
+            <h1> Privacy Policy </h1> <br></br>
+            <h2> 1. Limitations </h2>
+            <p> Roomeo or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Roomeo, 
+                even if Roomeo or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage.
+                Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.
+            </p>
+            <br></br>
+            <h2> 2. Revisions and Errata </h2>
+            <p> The materials appearing on Roomeo's Website may include technical, typographical, or photographic errors. Roomeo will not promise that any
+            of the materials in this Website are accurate, complete, or current. Roomeo may change the materials contained on its Website at any time without
+            notice. Roomeo does not make any commitment to update the materials.
+            </p>
+            <br></br>
+            <h2> 3. Links </h2>
+            <p> Roomeo has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link
+            does not imply endorsement by Roomeo of the site. The use of any linked website is at the user's own risk.
+            </p>
+            <br></br>
+            <h2> 4. Governing Law </h2>
+            <p> Any claim related to Roomes's Website shall be governed by the laws of Country without regards to its conflict of law provisions.
             </p>
         </div>
     )
