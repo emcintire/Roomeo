dece5e02a72fa91eaf80487d0cb08e5a14380fa7
Adora-A <aasin001@plattsburgh.edu>
2021-04-27 18:31:30 -0400

    Edited terms & conditions page

--- client/src/components/pages/Terms/terms.js
+++ client/src/components/pages/Terms/terms.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import './terms.css';
 // import { Link } from 'react-router-dom';
 
 function Terms() {
     return (
-        <div> 
-            <h1> Terms of Service</h1>
-            <p> 1. Our Audience
-                Roomeo is intended for individuals who are at least 18 years old and need to find a roommate for a shared living unit. Persons under the age of 18 are prohibited from creating Roommeo accounts.
-                You affirm that you are either at least 18 years of age, or an emancipated minor, or possess legal parental or guardian consent, and are fully able and competent to enter into the terms, conditions, obligations, affirmations, representations, and warranties set forth in these Terms, and to abide by and comply with these Terms. </p>
+        <div className='container'>  
+
+            <br></br>
+            <h1> Roomeo Terms and Conditions of Use</h1> <br></br>
+            <h2> 1. Terms </h2>
+            <p> By accessing this Website, accessible from Roomeo.com, you are agreeing to be bound by these Website 
+                Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. 
+                If you disagree with any of these terms, you are prohibited from accessing this site. 
+                The materials contained in this Website are protected by copyright and trade mark law.
+            </p>
+            <br></br>
+            <h2> 2. Use License </h2>
+            <p> Permission is granted to temporarily download one copy of the materials on Roomeo.com
+                for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+            </p>
+                <ul>
+                    <li>modify or copy the materials;</li>
+                    <li>use the materials for any commercial purpose or for any public display;</li>
+                    <li>attempt to reverse engineer any software contained on Company Name's Website;</li>
+                    <li>remove any copyright or other proprietary notations from the materials; or</li>
+                    <li>transferring the materials to another person or "mirror" the materials on any other server.</li>
+                </ul>
+            <br></br>
+            <h2> 3. Disclaimer </h2>
+            <p> All the materials on Roomeo.com are provided “as is”. Roomeo makes no warranties, may it be expressed or implied, therefore negates all other warranties. 
+            Furthermore, Roomeo does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.
+            </p>
+            <br></br>
+            <h2> 4. Site Terms of Use Modifications </h2>
+            <p> Roomeo may revise these Terms of Use for its Website at any time without prior notice.
+            By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.
+            </p>
         </div>
     )
 }
