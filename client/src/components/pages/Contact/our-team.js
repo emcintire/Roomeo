8c39e4ad31ff4033f97335d93c3ec16af4fb4080
Adora-A <aasin001@plattsburgh.edu>
2021-04-29 12:48:39 -0400

    Updated contact us page

--- client/src/components/pages/Contact/our-team.js
+++ client/src/components/pages/Contact/our-team.js
@@ -5,11 +5,18 @@ import React from 'react';
 function ourTeam() {
     return (
         <div>
-            <h1> Our Team </h1>
+            <br></br>
+            <h1> Our Team </h1> <br></br>
             <ol>
-                <li> 1. Adora Asinugo-Ejiogu (aasin001@plattsburgh.edu)</li>
-                <li> 2. Canaan Woods (cwood027@plattsburgh.edu)</li>
-                <li> 3. Everett McIntire (emcin003@plattsburgh.edu)</li>
+                <h3><li> 1. Adora Asinugo-Ejiogu - Frontend Developer</li></h3>
+                <p> Email: <a href="mailto:aasin001@plattsburgh.edu">aasin001@plattsburgh.edu</a></p>
+                <br></br>
+                <h3><li> 2. Canaan Woods - Development/Project Manager</li></h3>
+                <p>Email: <a href="mailto:cwood027@plattsburgh.edu">cwood027@plattsburgh.edu</a></p>
+                <br></br>
+                <h3><li> 3. Everett McIntire - Backend Developer</li></h3>
+                <p>Email: <a href="mailto:emcin003@plattsburgh.edu">mcin003@plattsburgh.edu</a></p>
+                <br></br>
             </ol>
         </div>
     )
